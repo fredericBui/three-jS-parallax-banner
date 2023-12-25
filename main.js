6741fe60c446ea797ff5ab11aed06f584adc23ba
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-12-25 14:18:21 +0100

    styling and add comment

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 import * as THREE from 'three'
 
+// Personalize the height (don't forget to change height of your banner in CSS also)
 const width = window.innerWidth,
   height = 600
 
 // init
 
 const camera = new THREE.PerspectiveCamera(50, width / height, 0.01, 10)
+// Change the distance of the camera
 camera.position.z = 3
 
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2) //Change the box size
 const material = new THREE.MeshNormalMaterial()
 
 const mesh = new THREE.Mesh(geometry, material)
@@ -19,13 +21,14 @@ scene.add(mesh)
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(width, height)
 renderer.setAnimationLoop(animation)
-renderer.setClearColor(0x000000, 0) // the default
+renderer.setClearColor(0x000000, 0)
+
+// Chose the element of the DOM that will display the 3D scene
 document.getElementById('threeJSBanner').appendChild(renderer.domElement)
 
 // animation
 
-console.log(window.scrollY)
-
+// Change the scroll effect to reach what you want
 document.onscroll = function () {
   camera.position.z = -window.scrollY / 120 + 3
   mesh.rotation.x = window.scrollY / 100
