4433ba0ee06e6c671b479668c6484828b09d2619
Frédéric Bui <bui.frederic.pro@gmail.com>
2024-01-01 23:18:00 +0100

    adding textures fonctionnality

--- threeJSInteractItem.js
+++ threeJSInteractItem.js
@@ -9,7 +9,7 @@ const width = style.width.replace(/\D/g, ''),
 
 // init
 
-const camera = new THREE.PerspectiveCamera(50, width / height, 0.01, 10)
+const camera = new THREE.PerspectiveCamera(40, width / height, 0.01, 10)
 // Change the distance of the camera
 camera.position.z = 0.75
 
@@ -19,6 +19,10 @@ const scene = new THREE.Scene()
 const ambientLight = new THREE.HemisphereLight('white', 'darkslategrey', 1)
 ambientLight.position.set(0, 1, 0)
 scene.add(ambientLight)
+
+const mainLight = new THREE.DirectionalLight('white', 0.5)
+mainLight.position.set(0, 10, 0)
+scene.add(mainLight)
 
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2) //Change the box size
 const mesh = new THREE.Mesh(geometry)
@@ -36,15 +40,26 @@ renderer.setClearColor(0x000000, 0)
 // Chose the element of the DOM that will display the 3D scene
 document.getElementById('threeJSInteractItem').appendChild(renderer.domElement)
 
-var btns = document.getElementsByClassName('actionBtn')
+var btns = document.getElementsByClassName('meshToonMaterialBtn')
 for (var i = 0; i < btns.length; i++) {
   var btn = btns[i]
   btn.onclick = function (btn) {
     mesh.material = new THREE.MeshToonMaterial({
       color: btn.target.attributes.color.value,
     })
-    // mesh.material = new THREE.MeshNormalMaterial()
-    // mesh.material = new THREE.MeshToonMaterial({ color: 'red' })
+  }
+}
+
+var btns = document.getElementsByClassName('meshBasicMaterialBtn')
+for (var i = 0; i < btns.length; i++) {
+  var btn = btns[i]
+  btn.onclick = function (btn) {
+    let uvTexture = new THREE.TextureLoader().load(
+      `textures/${btn.target.attributes.texture.value}.png`
+    )
+    mesh.material = new THREE.MeshBasicMaterial({
+      map: uvTexture,
+    })
   }
 }
 
